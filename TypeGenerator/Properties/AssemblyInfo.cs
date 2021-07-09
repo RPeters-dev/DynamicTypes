7364d91061e4234d2a5d10ddaeb43d9a928b1602
Robin Peters <robin.peters@live.de>
2021-07-09 12:35:57 +0200

    Update AssemblyInfo.cs

--- TypeGenerator/Properties/AssemblyInfo.cs
+++ TypeGenerator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TypeGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Robin Peters ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
